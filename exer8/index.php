dc021daf425b1a94170c158c9a1054f51f273eb3
natomaz <nathalytomazzini@yahoo.com.br>
2022-11-03 14:57:31 -0300

    adicionando exercicios finais

--- exer8/index.php
+++ exer8/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercício 7</title>
+    <title>Exercício 8</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -13,25 +13,19 @@
     <body>
         <div class="container box">
             <h1 style='font-family: "Abril Fatface", cursive; color: indigo; font-size: 50pt; text-align:center'>
-                Exercício 7 <br>
+                Exercício 8 <br>
             </h1>
                 <form action="index.php" method="get">
 
                 <div class="mb-3">
-                    <label for="n1">Digite o valor da compra:</label>
+                    <label for="n1">Horas gasta em uma viagem:</label>
                     <input type="number" class="form-control" name="n1">
                 </div>
 
                 <div class="mb-3">
-                    <label for="n2">Digite os dias de atraso:</label>
+                    <label for="n2">Velocidade média do seu percurso:</label>
                     <input type="number" class="form-control" name="n2">
                 </div>
-
-                <div class="mb-3">
-                    <label for="n3">Digite a taxa de atraso:</label>
-                    <input type="number" class="form-control" name="n3">
-                </div>
-
 
                 <div class="res">
                     <button class="btn btn-outline-secondary" name="rep" type="submit">CALCULE</button>
@@ -40,13 +34,20 @@
 
                 <div class="res">
                 <?php
-                $a=$_GET['n1'];
-                $b=$_GET['n2'];
-                $c=$_GET['n3'];
+                $tempogasto=$_GET['n1'];
+                $velocidademedia=$_GET['n2'];
+                $autonomia = 12;
+                
+                $distancia = $velocidademedia * $tempogasto;
+                $litrosusados = $distancia/$autonomia;
+                
 
-                $prest = $a + ($a*($c/100)*$b);
-
-                echo "<h1 style='color:indigo; font-size: 14pt; text-align:center'>Valor da prestação: ".$prest."</h1>";
+                echo "<h1 style='color:indigo; font-size: 14pt; text-align:center'>
+                Velocidade média: ".$velocidademedia." km/h.<br>
+                Tempo gasto: ".$tempogasto." horas.<br> 
+                Distância percorrida: ".$distancia." km. <br>
+                Quantidade de combustível utilizado: ".$litrosusados." litros.                
+                </h1>";
 
                 ?>
                 </div>
