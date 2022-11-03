6ad7baa61fbbedf487eade7246b3e2993f743d24
natomaz <nathalytomazzini@yahoo.com.br>
2022-11-03 14:24:37 -0300

    commit

--- exer4/index.php
+++ exer4/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercício 3</title>
+    <title>Exercício 5</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -13,22 +13,22 @@
     <body>
         <div class="container box">
             <h1 style='font-family: "Abril Fatface", cursive; color: indigo; font-size: 50pt; text-align:center'>
-                Exercício 3 <br>
+                Exercício 4 <br>
             </h1>
                 <form action="index.php" method="get">
 
                 <div class="mb-3">
-                    <label for="n1">Digite um número:</label>
+                    <label for="n1">Digite o valor de A:</label>
                     <input type="number" class="form-control" name="n1">
                 </div>
 
                 <div class="mb-3">
-                    <label for="n2">Digite um número:</label>
+                    <label for="n2">Digite o valor de B:</label>
                     <input type="number" class="form-control" name="n2">
                 </div>
 
                 <div class="mb-3">
-                    <label for="n3">Digite um número:</label>
+                    <label for="n3">Digite o valor de C:</label>
                     <input type="number" class="form-control" name="n3">
                 </div>
 
@@ -45,12 +45,17 @@
                 $b=$_GET['n2'];
                 $c=$_GET['n3'];
 
-                $r = ($a+$b) * ($a+$b);
-                $s = ($c+$b) * ($c+$b);
+                $d = ($b*$b)-((4*$a)*$c);
 
-                $d = ($r+$s)/2;
+                $x1 = (-$b + sqrt ($d)) / (2 * $a);
+                $x2 = (-$b - sqrt ($d)) / (2 * $a);
 
-                echo "<h1 style='color:blue; font-size: 14pt; text-align:center'>".$d."</h1>"; 
+                echo "<h1 style='color:indigo; font-size: 14pt; text-align:center'>O valor de a é ".$a."</h1>";
+                echo "<h1 style='color:indigo; font-size: 14pt; text-align:center'>O valor de b é ".$b."</h1>"; 
+                echo "<h1 style='color:indigo; font-size: 14pt; text-align:center'>O valor de c é ".$c."</h1>"; 
+                echo "<h1 style='color:indigo; font-size: 14pt; text-align:center'>O valor de delta é ".$d."</h1>"; 
+                echo "<h1 style='color:indigo; font-size: 14pt; text-align:center'>O valor de x1 é ".$x1."</h1>"; 
+                echo "<h1 style='color:indigo; font-size: 14pt; text-align:center'>O valor de x2 é ".$x2."</h1>"; 
                 ?>
                 </div>
                 </form>  
--- exer7/index.php
+++ exer7/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercício 3</title>
+    <title>Exercício 7</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -13,22 +13,22 @@
     <body>
         <div class="container box">
             <h1 style='font-family: "Abril Fatface", cursive; color: indigo; font-size: 50pt; text-align:center'>
-                Exercício 3 <br>
+                Exercício 7 <br>
             </h1>
                 <form action="index.php" method="get">
 
                 <div class="mb-3">
-                    <label for="n1">Digite um número:</label>
+                    <label for="n1">Digite o valor da sua hora aula:</label>
                     <input type="number" class="form-control" name="n1">
                 </div>
 
                 <div class="mb-3">
-                    <label for="n2">Digite um número:</label>
+                    <label for="n2">Digite o seu número de horas trabalhadas:</label>
                     <input type="number" class="form-control" name="n2">
                 </div>
 
                 <div class="mb-3">
-                    <label for="n3">Digite um número:</label>
+                    <label for="n3">Digite o percentual de desconto do INSS:</label>
                     <input type="number" class="form-control" name="n3">
                 </div>
 
@@ -38,19 +38,18 @@
 
                 </div>
 
-
                 <div class="res">
                 <?php
                 $a=$_GET['n1'];
                 $b=$_GET['n2'];
                 $c=$_GET['n3'];
 
-                $r = ($a+$b) * ($a+$b);
-                $s = ($c+$b) * ($c+$b);
+                $sb=$b*$a;
+                $desc=($sb*$c)/100;
+                $sl=$sb-$desc;
 
-                $d = ($r+$s)/2;
-
-                echo "<h1 style='color:blue; font-size: 14pt; text-align:center'>".$d."</h1>"; 
+                echo "<h1 style='color:indigo; font-size: 14pt; text-align:center'>Salário bruto: ".$sb."
+                <br>Desconto do INSS: ".$desc."<br>Salário líquido: ".$sl."  </h1>";
                 ?>
                 </div>
                 </form>  
