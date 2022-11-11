f78ccb17aeddd69d4d15d4555d1b3988e6cd573c
Nathaly Duarte <nathalytomazzini@yahoo.com.br>
2022-11-10 22:22:12 -0300

    crudagenda

---
+++ crudagenda/index.php
@@ -0,0 +1,67 @@
+<?php
+   session_start();
+   require 'connection.php';
+
+   // Verificar se foi enviando dados via POST
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $id = (isset($_POST["id"]) && $_POST["id"] != null) ? $_POST["id"] : "";
+    $nome = (isset($_POST["nome"]) && $_POST["nome"] != null) ? $_POST["nome"] : "";
+    $email = (isset($_POST["email"]) && $_POST["email"] != null) ? $_POST["email"] : "";
+    $celular = (isset($_POST["celular"]) && $_POST["celular"] != null) ? $_POST["celular"] : NULL;
+} else if (!isset($id)) {
+    // Se não se não foi setado nenhum valor para variável $id
+    $id = (isset($_GET["id"]) && $_GET["id"] != null) ? $_GET["id"] : "";
+    $nome = NULL;
+    $email = NULL;
+    $celular = NULL;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda</title>
+</head>
+<body>
+            <form action="?act=save" method="POST" name="form1" >
+                <h1>Agenda de contatos</h1>
+                <hr>
+                <input type="hidden" name="id" <?php
+                // Preenche o id no campo id com um valor "value"
+                if (isset($id) && $id != null || $id != "") {
+                    echo "value=\"{$id}\"";
+                }
+                ?> />
+                Nome:
+                <input type="text" name="nome" <?php
+                // Preenche o nome no campo nome com um valor "value"
+                if (isset($nome) && $nome != null || $nome != ""){
+                    echo "value=\"{$nome}\"";
+                }
+                ?> />
+                E-mail:
+                <input type="text" name="email" <?php
+                // Preenche o email no campo email com um valor "value"
+                if (isset($email) && $email != null || $email != ""){
+                    echo "value=\"{$email}\"";
+                }
+                ?> />
+                Celular:
+                <input type="text" name="celular" <?php
+                // Preenche o celular no campo celular com um valor "value"
+                if (isset($celular) && $celular != null || $celular != ""){
+                    echo "value=\"{$celular}\"";
+                }
+                ?> />
+                <input type="submit" value="salvar" />
+                <input type="reset" value="Novo" />
+                <hr>
+            </form>
+
+    
+</body>
+</html>
